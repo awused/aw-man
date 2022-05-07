83324aa51aec680054f9cf70411c623250083ae6
Desuwa <desuwa@desuwa.net>
2022-05-06 22:52:38 -0700

    Disable system-jxl on Windows and avoid probing it

--- build.rs
+++ build.rs
@@ -2,4 +2,9 @@ extern crate metadeps;
 
 fn main() {
     metadeps::probe().unwrap();
+    #[cfg(not(windows))]
+    pkg_config::Config::new()
+        .atleast_version("0.3.7")
+        .probe("libjxl")
+        .unwrap();
 }
