fbfb097163082d3e9386fb9402cd514b48b43e76
Desuwa <desuwa@desuwa.net>
2023-03-21 19:06:28 -0700

    Comment about rotation during downscaling

--- src/resample.rs
+++ src/resample.rs
@@ -298,6 +298,11 @@ fn horizontal_par_sample<const N: usize, const S: usize, K: Fn(f32) -> f32 + Syn
         },
     );
 
+    // After benchmarking, producing a rotated image and rotating it here is faster than using
+    // unsafe to skip this step. Probably due to SIMD optimizations and cache coherency, may change
+    // depending on CPU architecture.
+    // Might be possible to fix that, but it's unlikely to pay off compared to just switching to
+    // OpenCL.
     let mut rotated = vec![0; new_width as usize * height as usize * N];
 
     rotated
