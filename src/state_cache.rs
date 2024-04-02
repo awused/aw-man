0c70994fc7aa50ea718520b223d2ce4cb35a4e6a
Desuwa <desuwa@desuwa.net>
2024-04-02 10:26:09 -0700

    Minor state_cache cleanup

--- src/state_cache.rs
+++ src/state_cache.rs
@@ -18,7 +18,7 @@ pub struct State {
 }
 
 static CACHE_FILE: Lazy<Option<PathBuf>> = Lazy::new(|| {
-    let mut cache = dirs::cache_dir()?;
+    let mut cache = dirs::state_dir().or_else(dirs::cache_dir)?;
     cache.push("aw-man");
     cache.push("saved.json");
     Some(cache)
@@ -52,19 +52,20 @@ pub fn save_settings(s: State) {
         return;
     }
 
-    let Some(cache) = (*CACHE_FILE).as_ref() else { return };
+    let Some(cache) = (*CACHE_FILE).as_ref() else {
+        return;
+    };
 
     let serialized = serde_json::to_string(&s).unwrap();
 
     if let Some(p) = cache.parent() {
         if let Err(e) = fs::create_dir_all(p) {
-            error!("Unable to create directory to save window state: {e}");
-            return;
+            return error!("Unable to create directory to save window state: {e}");
         }
     }
 
     if let Err(e) = fs::write(cache, serialized) {
-        error!("Could not save window state: {e}");
+        return error!("Could not save window state: {e}");
     }
 
     trace!("Wrote window state cache for next run.");
